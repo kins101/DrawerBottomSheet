aee9c5b68e385a022c534ff3fc9a9a78bb720886
kins101 <s.kin@cluster.mu>
2023-10-27 02:19:55 -0400

    Add presentationDetents test code

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -9,22 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isShowing: Bool = false
-    
+    @State private var isPresented: Bool = false
+
     var body: some View {
         ZStack {
             Color.blue
                 .ignoresSafeArea()
             
-            Button {
-                withAnimation {
-                    isShowing.toggle()
+            VStack(spacing: 30) {
+                Button {
+                    withAnimation {
+                        isPresented.toggle()
+                    }
+                } label: {
+                    Text("PresentationDetents")
+                        .foregroundColor(.white)
                 }
-            } label: {
-                Text("Click")
-                    .foregroundColor(.white)
+                
+                Button {
+                    withAnimation {
+                        isShowing.toggle()
+                    }
+                } label: {
+                    Text("Custom Sheet")
+                        .foregroundColor(.white)
+                }
             }
-
+            
             DrawerBottomSheet(isShowing: $isShowing, content: AnyView(TestView()))
+        }
+        .sheet(isPresented: $isPresented) {
+            ScrollView {
+                TestView()
+            }
+            .padding()
+            .presentationDetents([.fraction(0.8), .large])
         }
     }
 }
